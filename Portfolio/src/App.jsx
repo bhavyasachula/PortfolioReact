8d2fcd6475103bb141cebba9d067a6eb10dc39cd
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-17 21:48:54 +0530

    edit : imported the tunnel animation

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,12 +1,43 @@
 import { useState } from 'react'
-
+import LightTunnel from './components/LightTunnel.jsx'; 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <LightTunnel
+    cableColor="#A855F7"
+    pulseColor="#A855F7"
+    tunnelColor="#5227FF"
+    tunnelOpacity={0}
+    speed={0.1}
+    flowDirection="outward"
+    pulseSpeed={2}
+    pulseLength={0.28}
+    pulseBlend={1}
+    pulseWidth={1}
+    cableCount={20}
+    thickness={0.35}
+    rimWidth={0.15}
+    waviness={0.3}
+    sway={0.5}
+    size={1}
+    centerX={0}
+    centerY={0}
+    glow={1}
+    fadeNear={0.5}
+    fadeFar={2}
+    brightness={1}
+    colorVariance
+    grain
+    grainIntensity={0.05}
+    opacity={1}
+    mouseInteraction
+    mouseStrength={0.1}
+  />
+</div>
     </>
   )
 }

0d5d62f5056c39ad67b3b0f37cb10ecf8307d17c
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-20 23:28:54 +0530

    checks : navvbar

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
    <>
     <div className="main" style={{ width: '100%', height: '600px', position: 'relative'  }}>
   <nav className='navbar'>
+    <div className="nav-logo">Bhavya</div>
   <ul>
     <li><a href="#home">Home</a></li>
     <li><a href="#about">About</a></li>

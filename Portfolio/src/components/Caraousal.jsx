d2483e1dbffb1a48a2384e18224a218e8391af1a
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-21 22:12:00 +0530

    feat : Caraousal file

---
+++ Portfolio/src/components/Caraousal.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './caraousal.css'
+function Caraousal() {
+  return (
+    <>
+    <div className='Caraousal'>
+        <div className='Group'>
+             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732221.png" alt="" /></div>   
+            <div className="card"><img src='https://cdn-icons-png.flaticon.com/512/5968/5968958.png' alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968776.png" alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732217.png" alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/888/888835.png" alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968382.png" alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/6124/6124992.png" alt="" /></div>
+            <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/152/152752.png" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/ORCL.webp" alt="" /></div>
+            <div className="card"><img src="	https://companiesmarketcap.com/img/company-logos/64/NFLX.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/SPOT.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/ABNB.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/NVDA.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/SPCX.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/TSLA.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/TCS.NS.webp" alt="" /></div>
+            
+        </div>
+         <div className='Group'>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732221.png" alt="" /></div>   
+            <div className="card"><img src='https://cdn-icons-png.flaticon.com/512/5968/5968958.png' alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968776.png" alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/732/732217.png" alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/888/888835.png" alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968382.png" alt="" /></div>
+            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/6124/6124992.png" alt="" /></div>
+            <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/152/152752.png" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/ORCL.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/NFLX.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/SPOT.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/ABNB.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/NVDA.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/SPCX.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/TSLA.webp" alt="" /></div>
+            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/TCS.NS.webp" alt="" /></div>
+      
+        </div>
+    </div>  
+    </>
+  )
+}
+
+export default Caraousal

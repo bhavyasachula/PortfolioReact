a8315253e24bd5b2d1b6068274513fe36b6a5ab2
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-21 21:50:35 +0530

    feat : Tech stack

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
   <ul>
     <li><a href="#home">Home</a></li>
     <li><a href="#about">About</a></li>
+    <li><a href="#TechStack">Tech Stack</a></li>
     <li><a href="#projects">Work</a></li>
     <li><a href="#contact" style={{background:'rgba(212, 4, 249, 0.267)'}}>Let's Talk</a></li>
   </ul>

36ee01940c2e86127af193f0b81c13153d19984f
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-21 21:49:57 +0530

    feat : replace project with work

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   <ul>
     <li><a href="#home">Home</a></li>
     <li><a href="#about">About</a></li>
-    <li><a href="#projects">Projects</a></li>
+    <li><a href="#projects">Work</a></li>
     <li><a href="#contact" style={{background:'rgba(212, 4, 249, 0.267)'}}>Let's Talk</a></li>
   </ul>
 </nav>

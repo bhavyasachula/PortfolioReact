d0fa8f18dc163d52fc86837865724cb2162b3b77
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-21 21:59:50 +0530

    feat : Increased the height of main content

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
    <>
   
-    <div className="main" style={{ width: '100%', height: '600px', position: 'relative'  }}>
+  <div className="main" style={{ width: '100%', height: '750px', position: 'relative'  }}>
 
   <nav className='navbar'>
     <div className="nav-logo">BHAVYA</div>

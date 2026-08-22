9aa679f9570c5d798e876f33fe0216347b4ec0b0
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-22 20:17:52 +0530

    Feat : Modularize the navbar

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -12,16 +12,7 @@ function App() {
   
   <div className="main" style={{ width: '100%', height: '750px', position: 'relative'  }}>
 
-  <nav className='navbar'>
-    <div className="nav-logo">BHAVYA</div>
-  <ul>
-    <li><a href="#home">Home</a></li>
-    <li><a href="#about">About</a></li>
-    <li><a href="#TechStack">Tech Stack</a></li>
-    <li><a href="#projects">Work</a></li>
-    <li><a href="#contact" style={{background:'rgba(212, 4, 249, 0.267)'}}>Let's Talk</a></li>
-  </ul>
-</nav>
+<Navbar></Navbar>
  <LightTunnel
     cableColor="#A855F7"
     pulseColor="#A855F7"

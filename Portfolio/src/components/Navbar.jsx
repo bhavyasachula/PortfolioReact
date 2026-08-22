b5e6bca0e056a44dce362e25f91117a294d9a90c
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-22 20:18:18 +0530

    feat : nav

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-<nav className='navbar'>
-  <div className="nav-logo">Bhavya</div>
+  <nav className='navbar'>
+    <div className="nav-logo">BHAVYA</div>
   <ul>
     <li><a href="#home">Home</a></li>
     <li><a href="#about">About</a></li>
-    <li><a href="#projects">Projects</a></li>
-    <li><a href="#contact">Contact</a></li>
+    <li><a href="#TechStack">Tech Stack</a></li>
+    <li><a href="#projects">Work</a></li>
+    <li><a href="#contact" style={{background:'rgba(212, 4, 249, 0.267)'}}>Let's Talk</a></li>
   </ul>
 </nav>

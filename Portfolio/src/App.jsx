26608abd72434fc35f6ee25e2af8cb5b9f2c2340
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-22 20:30:42 +0530

    feat : Checking new theme

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import LightTunnel from './components/LightTunnel.jsx'; 
 import Caraousal from './components/Caraousal.jsx';
 import Navbar from './components/Navbar.jsx';
+import FaultyTerminal from './components/FaultyTerminal.jsx';
 import './App.css'
 function App() {
 
@@ -14,35 +15,24 @@ function App() {
   <div className="main" style={{ width: '100%', height: '750px', position: 'relative'  }}>
 
   <Navbar></Navbar>
- <LightTunnel
-    cableColor="#A855F7"
-    pulseColor="#A855F7"
-    tunnelColor="#5227FF"
-    tunnelOpacity={0}
-    speed={0.1}
-    flowDirection="outward"
-    pulseSpeed={2}
-    pulseLength={0.28}
-    pulseBlend={1}
-    pulseWidth={1}
-    cableCount={20}
-    thickness={0.35}
-    rimWidth={0.15}
-    waviness={0.3}
-    sway={0.5}
-    size={1}
-    centerX={0}
-    centerY={0}
-    glow={1}
-    fadeNear={0.5}
-    fadeFar={2}
-    brightness={1}
-    colorVariance
-    grain
-    grainIntensity={0.05}
-    opacity={1}
-    mouseInteraction
-    mouseStrength={0.1}
+   <FaultyTerminal
+    scale={1.5}
+    gridMul={[2, 1]}
+    digitSize={1.2}
+    timeScale={0.5}
+    pause={false}
+    scanlineIntensity={0.5}
+    glitchAmount={1}
+    flickerAmount={1}
+    noiseAmp={1}
+    chromaticAberration={0}
+    dither={0}
+    curvature={0.1}
+    tint="#7C3AED"
+    mouseReact
+    mouseStrength={0.5}
+    pageLoadAnimation
+    brightness={0.6}
   />
  <div className='HeroText' onClick={onHeroClick}>Hi,I m BHAVYA</div> 
   </div>

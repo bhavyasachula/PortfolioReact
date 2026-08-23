9828f551fddf1862bb18ca98c600db85cba12af7
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-23 23:37:45 +0530

    feat : tech stack logos

--- Portfolio/src/components/Caraousal.jsx
+++ Portfolio/src/components/Caraousal.jsx
@@ -9,7 +9,7 @@ function Caraousal() {
             <div className="card"><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5-Uz-AfifXnUmf7H9Uf3AZkelAOZlDFCYMptKI3iGx6AfZGAotoxFyrG75C5vKT4WbTq4cgwrdklPkjRTz2cEHWkAlzN2RaWa5zMl2A&s=10' alt="" /></div>
             <div className="card"><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTqrOXX6aDLQETYHcaJWSdReybJBEgLNN3NPwWx8P0iRGnfKkwP" alt="" /></div>
             <div className="card"><img src="https://storage.googleapis.com/chilipiper-cloud-static/workspace-logos/langchain.com/bd2bda48-ec1b-480c-8468-98e43909e079-93ca5a37-84a1-4aec-ab4e-86c54a01d315.png" alt="" /></div>
-            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/888/888835.png" alt="" /></div>
+            <div className="card"><img src="../src/assets/nodejslogo.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968382.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/6124/6124992.png" alt="" /></div>
             <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/152/152752.png" alt="" /></div>
@@ -28,7 +28,7 @@ function Caraousal() {
             <div className="card"><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5-Uz-AfifXnUmf7H9Uf3AZkelAOZlDFCYMptKI3iGx6AfZGAotoxFyrG75C5vKT4WbTq4cgwrdklPkjRTz2cEHWkAlzN2RaWa5zMl2A&s=10' alt="" /></div>
             <div className="card"><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTqrOXX6aDLQETYHcaJWSdReybJBEgLNN3NPwWx8P0iRGnfKkwP" alt="" /></div>
             <div className="card"><img src="https://storage.googleapis.com/chilipiper-cloud-static/workspace-logos/langchain.com/bd2bda48-ec1b-480c-8468-98e43909e079-93ca5a37-84a1-4aec-ab4e-86c54a01d315.png" alt="" /></div>
-            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/888/888835.png" alt="" /></div>
+            <div className="card"><img src="../src/assets/nodejslogo.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968382.png" alt="" /></div>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/6124/6124992.png" alt="" /></div>
             <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/152/152752.png" alt="" /></div>

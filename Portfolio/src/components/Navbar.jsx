c258e55427ef4b71333b32ecd23680e68f26c829
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-23 20:04:27 +0530

    feat : Changed the text with image

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 
 function Navbar(){
-  return(<nav className='navbar'>
-    <div className="nav-logo">BHAVYA</div>
+  return(
+  <nav className='navbar'>
+    <div className="nav-logo"><img src="../src/assets/galaxyStar.png" alt="" /></div>
   <ul>
     <li><a href="#home">Home</a></li>
     <li><a href="#about">About</a></li>

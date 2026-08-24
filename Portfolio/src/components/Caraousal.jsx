c1c5289dc1a9d25cbd7ef3eb7a3d91b537ded3ff
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-24 10:15:29 +0530

    feat : added languages logos

--- Portfolio/src/components/Caraousal.jsx
+++ Portfolio/src/components/Caraousal.jsx
@@ -6,39 +6,39 @@ function Caraousal() {
     <div className='Caraousal'>
         <div className='Group'>
              <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968350.png" alt="" /></div>   
-            <div className="card"><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5-Uz-AfifXnUmf7H9Uf3AZkelAOZlDFCYMptKI3iGx6AfZGAotoxFyrG75C5vKT4WbTq4cgwrdklPkjRTz2cEHWkAlzN2RaWa5zMl2A&s=10' alt="" /></div>
+            <div className="card"><img src='../src/assets/reactjslogo.png' alt="" /></div>
             <div className="card"><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTqrOXX6aDLQETYHcaJWSdReybJBEgLNN3NPwWx8P0iRGnfKkwP" alt="" /></div>
             <div className="card"><img src="https://storage.googleapis.com/chilipiper-cloud-static/workspace-logos/langchain.com/bd2bda48-ec1b-480c-8468-98e43909e079-93ca5a37-84a1-4aec-ab4e-86c54a01d315.png" alt="" /></div>
             <div className="card"><img src="../src/assets/nodejslogo.png" alt="" /></div>
             <div className="card"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8cxgwkzgZl9Z7OFj04lWIEZJCD3QeGH_YhdVcjcRAoA&s=10" alt="" /></div>
-            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/6124/6124992.png" alt="" /></div>
-            <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/152/152752.png" alt="" /></div>
-            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/ORCL.webp" alt="" /></div>
-            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/NFLX.webp" alt="" /></div>
+            <div className="card"><img src="https://avatars.githubusercontent.com/u/10566080?s=280&v=4" alt="" /></div>
+            <div className='card'><img src="https://avatars.githubusercontent.com/u/18133?s=280&v=4" alt="" /></div>
+            <div className="card"><img src="https://raw.githubusercontent.com/lobehub/lobe-icons/refs/heads/master/packages/static-png/dark/langgraph-color.png" alt="" /></div>
+            <div className="card"><img src="../src/assets/pytorchlog.png" alt="" /></div>
             <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/SPOT.webp" alt="" /></div>
             <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/ABNB.webp" alt="" /></div>
-            <div className="card"><img src="" alt="" /></div>
+            <div className="card"><img src="../src/assets/langsmithlogo.png" alt="" /></div>
             <div className="card"><img src="https://avatars.githubusercontent.com/u/156354296?s=280&v=4" alt="" /></div>
-            <div className="card"><img src="https://cdn.prod.website-files.com/66754aa69a5d872183713f9c/672dfc530eff071e52a2ed44_PostgreSQL-Logo.wine.png" alt="" /></div>
+            <div className="card"><img src="https://www.altnix.com/_next/static/media/postgreSQL.857496f6.png" alt="" /></div>
             <div className="card"><img src="https://assets.codeguru.com/uploads/2003/02/C-tutorials.jpg?f=jpeg" alt="" /></div>
             
         </div>
          <div className='Group'>
             <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968350.png" alt="" /></div>   
-            <div className="card"><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5-Uz-AfifXnUmf7H9Uf3AZkelAOZlDFCYMptKI3iGx6AfZGAotoxFyrG75C5vKT4WbTq4cgwrdklPkjRTz2cEHWkAlzN2RaWa5zMl2A&s=10' alt="" /></div>
+            <div className="card"><img src='../src/assets/reactjslogo.png' alt="" /></div>
             <div className="card"><img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTqrOXX6aDLQETYHcaJWSdReybJBEgLNN3NPwWx8P0iRGnfKkwP" alt="" /></div>
             <div className="card"><img src="https://storage.googleapis.com/chilipiper-cloud-static/workspace-logos/langchain.com/bd2bda48-ec1b-480c-8468-98e43909e079-93ca5a37-84a1-4aec-ab4e-86c54a01d315.png" alt="" /></div>
             <div className="card"><img src="../src/assets/nodejslogo.png" alt="" /></div>
             <div className="card"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8cxgwkzgZl9Z7OFj04lWIEZJCD3QeGH_YhdVcjcRAoA&s=10" alt="" /></div>
-            <div className="card"><img src="https://cdn-icons-png.flaticon.com/512/6124/6124992.png" alt="" /></div>
-            <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/152/152752.png" alt="" /></div>
-            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/ORCL.webp" alt="" /></div>
-            <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/NFLX.webp" alt="" /></div>
+            <div className="card"><img src="https://avatars.githubusercontent.com/u/10566080?s=280&v=4" alt="" /></div>
+            <div className='card'><img src="https://avatars.githubusercontent.com/u/18133?s=280&v=4" alt="" /></div>
+            <div className="card"><img src="https://raw.githubusercontent.com/lobehub/lobe-icons/refs/heads/master/packages/static-png/dark/langgraph-color.png" alt="" /></div>
+            <div className="card"><img src="../src/assets/pytorchlog.png" alt="" /></div>
             <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/SPOT.webp" alt="" /></div>
             <div className="card"><img src="https://companiesmarketcap.com/img/company-logos/64/ABNB.webp" alt="" /></div>
-            <div className="card"><img src="" alt="" /></div>
+            <div className="card"><img src="../src/assets/langsmithlogo.png" alt="" /></div>
             <div className="card"><img src="https://avatars.githubusercontent.com/u/156354296?s=280&v=4" alt="" /></div>
-            <div className="card"><img src="https://cdn.prod.website-files.com/66754aa69a5d872183713f9c/672dfc530eff071e52a2ed44_PostgreSQL-Logo.wine.png" alt="" /></div>
+            <div className="card"><img src="https://www.altnix.com/_next/static/media/postgreSQL.857496f6.png" alt="" /></div>
             <div className="card"><img src="https://assets.codeguru.com/uploads/2003/02/C-tutorials.jpg?f=jpeg" alt="" /></div>
       
         </div>

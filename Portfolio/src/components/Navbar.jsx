620e96d1bbbf849e8f608f8336db7367a546e2a1
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-24 16:57:19 +0530

    imported the image

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,15 +1,28 @@
+import galaxyStar from "../assets/galaxyStar.png";
 
-function Navbar(){
-  return(
-  <nav className='navbar'>
-    <div className="nav-logo"><img src="../src/assets/galaxyStar.png" alt="" /></div>
-  <ul>
-    <li><a href="#home">Home</a></li>
-    <li><a href="#about">About</a></li>
-    <li><a href="#TechStack">Tech Stack</a></li>
-    <li><a href="#projects">Work</a></li>
-    <li><a href="#contact" style={{background:'rgba(212, 4, 249, 0.267)'}}>Let's Talk</a></li>
-  </ul>
-</nav>)
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">
+        <img src={galaxyStar} alt="Galaxy Star" />
+      </div>
+
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#TechStack">Tech Stack</a></li>
+        <li><a href="#projects">Work</a></li>
+        <li>
+          <a
+            href="#contact"
+            style={{ background: "rgba(212, 4, 249, 0.267)" }}
+          >
+            Let's Talk
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+
 export default Navbar;

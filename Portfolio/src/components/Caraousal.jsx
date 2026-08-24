b8d4240186a07f98e4c794ecae479ba66bbc7b1c
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-24 23:46:46 +0530

    logo : Chroma db

--- Portfolio/src/components/Caraousal.jsx
+++ Portfolio/src/components/Caraousal.jsx
@@ -19,6 +19,7 @@ function Caraousal() {
             <div className="card"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Claude_AI_symbol.svg/3840px-Claude_AI_symbol.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail" alt="" /></div>
              <div className="card"><img src="https://static.vecteezy.com/system/resources/previews/055/687/065/non_2x/gemini-google-icon-symbol-logo-free-png.png" alt="" /></div>
             <div className="card"><img src="../src/assets/langsmithlogo.png" alt="" /></div>
+            <div className="card"><img src="https://ubos.tech/wp-content/uploads/2023/11/chroma.png" alt="" /></div>
             <div className="card"><img src="https://avatars.githubusercontent.com/u/156354296?s=280&v=4" alt="" /></div>
             <div className="card"><img src="https://cdn.iconscout.com/icon/free/png-256/free-postgresql-icon-svg-download-png-1175119.png?f=webp&w=128" alt="" /></div>
             <div className="card"><img src="https://assets.codeguru.com/uploads/2003/02/C-tutorials.jpg?f=jpeg" alt="" /></div>
@@ -39,6 +40,7 @@ function Caraousal() {
             <div className="card"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Claude_AI_symbol.svg/3840px-Claude_AI_symbol.svg.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail" alt="" /></div>
             <div className="card"><img src="https://static.vecteezy.com/system/resources/previews/055/687/065/non_2x/gemini-google-icon-symbol-logo-free-png.png" alt="" /></div>
             <div className="card"><img src="../src/assets/langsmithlogo.png" alt="" /></div>
+            <div className="card"><img src="https://ubos.tech/wp-content/uploads/2023/11/chroma.png" alt="" /></div>
             <div className="card"><img src="https://avatars.githubusercontent.com/u/156354296?s=280&v=4" alt="" /></div>
             <div className="card"><img src="https://cdn.iconscout.com/icon/free/png-256/free-postgresql-icon-svg-download-png-1175119.png?f=webp&w=128" alt="" /></div>
             <div className="card"><img src="https://assets.codeguru.com/uploads/2003/02/C-tutorials.jpg?f=jpeg" alt="" /></div>

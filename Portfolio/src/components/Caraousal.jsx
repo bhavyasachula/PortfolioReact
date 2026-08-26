fb14e697d48525c59292449d576284ea4ea6d2a4
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-26 16:10:50 +0530

    edit : fixed pandas logo

--- Portfolio/src/components/Caraousal.jsx
+++ Portfolio/src/components/Caraousal.jsx
@@ -26,7 +26,7 @@ function Caraousal() {
             <div className="card"><img src="https://assets.codeguru.com/uploads/2003/02/C-tutorials.jpg?f=jpeg" alt="" /></div>
             <div className="card"><img src="https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHHFT949fUipzkiFOBH3fAiZZUCdYojwUyX2aTonS1aIwMrx6NUIsHfUHSLzjGJFxxvHMT0riO5Ze2r4kAINc_2QgRT1de06pviP2kjjyzFLpCTZZEk1s5nN1tTMNrZUQs52mBPXbxUYT04uiFQgRxAc-&format=source" alt="" /></div>
             <div className="card"><img src="https://avatars.slack-edge.com/2021-07-28/2335856319233_b1f6442290972bfa5498_512.png" alt="" /></div>
-            <div className="card"><img src="https://images.opencollective.com/pandas/6e5c060/logo/256.png" alt="" /></div>
+            <div className="card"><img src="https://images.seeklogo.com/logo-png/45/1/pandas-logo-png_seeklogo-459105.png" alt="" /></div>
             <div className="card"><img src="https://media.licdn.com/dms/image/v2/D5612AQFSTglfKdI9eg/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1708971797430?e=2147483647&v=beta&t=iZKe_LBwW0NSHcz1V9_LsKskeje_BYusBCoctWYnWJ0" alt="" /></div> 
             <div className="card"><img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/github-white-icon.png" alt="" /></div>
             <div className="card"><img src="https://i0.wp.com/ahex.co/wp-content/uploads/2022/06/d3.png?fit=400%2C400&ssl=1" alt="" /></div>
@@ -54,7 +54,7 @@ function Caraousal() {
             <div className="card"><img src="https://assets.codeguru.com/uploads/2003/02/C-tutorials.jpg?f=jpeg" alt="" /></div>
             <div className="card"><img src="https://images-eds-ssl.xboxlive.com/image?url=4rt9.lXDC4H_93laV1_eHHFT949fUipzkiFOBH3fAiZZUCdYojwUyX2aTonS1aIwMrx6NUIsHfUHSLzjGJFxxvHMT0riO5Ze2r4kAINc_2QgRT1de06pviP2kjjyzFLpCTZZEk1s5nN1tTMNrZUQs52mBPXbxUYT04uiFQgRxAc-&format=source" alt="" /></div>
             <div className="card"><img src="https://avatars.slack-edge.com/2021-07-28/2335856319233_b1f6442290972bfa5498_512.png" alt="" /></div>
-            <div className="card"><img src="https://images.opencollective.com/pandas/6e5c060/logo/256.png" alt="" /></div> 
+            <div className="card"><img src="https://images.seeklogo.com/logo-png/45/1/pandas-logo-png_seeklogo-459105.png" alt="" /></div> 
             <div className="card"><img src="https://media.licdn.com/dms/image/v2/D5612AQFSTglfKdI9eg/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1708971797430?e=2147483647&v=beta&t=iZKe_LBwW0NSHcz1V9_LsKskeje_BYusBCoctWYnWJ0" alt="" /></div> 
             <div className="card"><img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/github-white-icon.png" alt="" /></div> 
             <div className="card"><img src="https://i0.wp.com/ahex.co/wp-content/uploads/2022/06/d3.png?fit=400%2C400&ssl=1" alt="" /></div>

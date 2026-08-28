563649fb7af8cc760f155c22dd35408eaba1fb82
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-28 17:48:43 +0530

    edit : removed resume from navbar

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -12,14 +12,7 @@ function Navbar() {
         <li><a href="#about">About</a></li>
         <li><a href="#TechStack">Tech Stack</a></li>
         <li><a href="#projects">Work</a></li>
-        <li>
-          <a
-            href="#contact"
-            style={{ background: "rgba(212, 4, 249, 0.267)" }}
-          >
-            Let's Talk
-          </a>
-        </li>
+        <li><a href="#contact" style={{ background: "rgba(212, 4, 249, 0.267)" }}>Let's Talk</a></li>
       </ul>
     </nav>
   );

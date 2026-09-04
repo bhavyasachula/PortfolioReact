a8c83c4a58287e24f9e9491f36e660864e584384
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-04 23:12:19 +0530

    Adding content

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import "../App.css"
 function Contact() {
   return (
     <>
-    <div className='Contact h-100 w-100 '></div>
+    <div className='flex contact h-[800px] w-[100%] p-1 border-1 border-white text-white justify-center items-center'>
+      <div className='flex contact-inner h-[600px] w-[50%] border-1 border-white text-white '>
+
+      </div>
+    </div>
     </>
   )
 }

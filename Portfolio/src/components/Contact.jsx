3137d1e03f70fd1c7ae70fde42d538f922771b1c
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-04 23:30:59 +0530

    Completed desc part

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import "../App.css"
+import './contact.css'
 function Contact() {
   return (
     <>
     <div className='flex contact h-[800px] w-[100%] p-1 border-1 border-white text-white justify-center items-center'>
-      <div className='flex contact-inner h-[600px] w-[50%] border-1 border-white text-white '>
-
+      <div className='contact-inner flex flex-col h-[600px] w-[50%] border-1 border-white text-white '>
+          <div className='getinTouch'>Get in Touch</div>
+          <div className='getinDesc'>Intersted in collaborating in a project, discussing an engineering role, or just want to say hi? Send a note or reach out directly</div>
+          <div className='getinLoc'> ijfnoksdnfjsniof</div>
       </div>
     </div>
     </>

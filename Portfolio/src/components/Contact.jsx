fa5fd3c822cc7ae8581e03cb33fa62b1760f1930
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-07 21:51:31 +0530

    removed unnessasary border

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -17,16 +17,16 @@ function Contact() {
             <label htmlFor="" >Email</label>
             <div className='EmailComponent flex border-1 border-white'>
               <div className='emailText'>bhavyasachula07@gmail.com</div>
-              <button className='text-white  border-1 border-white rounded-md' onClick={handleCopy}>Copy</button>
+              <button className='text-white rounded-md' onClick={handleCopy}>Copy</button>
             </div> 
           <div className='getinDesc '>Intersted in collaborating in a project, discussing an engineering role, or just want to say hi? Send a note or reach out directly</div>
           <div className='getinLoc grid gap-1  w-[100%]'> 
-          <div className='getcard  '>
+          <div className='getcard'>
             <div className='title'>Location</div>
             <div className='text text-[25px]'>Ahmedabad,Gujarat</div>
             <div className='title'>IST (UTC +05:30)</div>
           </div>
-          <div className='getcard  '>
+          <div className='getcard'>
             <div className='title'>Availablity</div>
             <div className='text text-[25px]'>Full time & Advisory</div>
             <div className='title'>Open to select projects</div>
@@ -36,25 +36,25 @@ function Contact() {
             <div className="socialcard">
               <div>Github</div>
               <div>
-                <svg width="22px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
             </div>
            <div className="socialcard">
               <div>LinkedIn</div>
               <div>
-                <svg width="22px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
             </div>
             <div className="socialcard">
               <div>X /Twitter</div>
               <div>
-                <svg width="22px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
             </div>
             <div className="socialcard">
               <div>Discord</div>
               <div>
-                <svg width="22px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
             </div>
           </div>

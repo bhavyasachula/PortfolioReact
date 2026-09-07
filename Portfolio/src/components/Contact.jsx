78d8bd76db04c990e151d24185eb9ef5d7495c49
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-07 22:18:04 +0530

    edit : onclick redirection

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -7,6 +7,9 @@ async function handleCopy(){
 }
 
 function Contact() {
+  function githubDirect(){
+    window.open("https://www/github.com/bhavyasachula");
+  }
   const [copied,setCopied] = useState(false);
   return (
     <>
@@ -34,12 +37,11 @@ function Contact() {
           </div>
           <div className='socials mt-10 '>
             <div className="socialcard">
-              <a href="https://www.github.com/bhavyasachula">
-              <div>Github</div>
+              <div onClick={githubDirect}>Github</div>
               <div>
                 <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
- </a>
+
             </div>
            <div className="socialcard">
               <div>LinkedIn</div>

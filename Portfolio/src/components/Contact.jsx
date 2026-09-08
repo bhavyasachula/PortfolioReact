2b1cde1684feebf969fc2113bf715c14bf6e8239
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-08 21:47:06 +0530

    Added an anchor tag to divs

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -36,31 +36,31 @@ function Contact() {
           </div>
           </div>
           <div className='socials mt-10 '>
-            <div className="socialcard">
-              <div onClick={githubDirect}>Github</div>
-              <div>
+            <a className="socialcard">
+              <div>Github</div>
+              <div >
                 <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
 
-            </div>
-           <div className="socialcard">
+            </a>
+           <a className="socialcard">
               <div>LinkedIn</div>
               <div>
                 <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
-            </div>
-            <div className="socialcard">
+            </a>
+            <a className="socialcard">
               <div>X /Twitter</div>
               <div>
                 <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
-            </div>
-            <div className="socialcard">
+            </a>
+            <a className="socialcard">
               <div>Discord</div>
               <div>
                 <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
-            </div>
+            </a>
           </div>
       </div>
     </div>

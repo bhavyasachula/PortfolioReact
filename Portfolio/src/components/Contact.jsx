680df15ffe84260f00cf39f54858e6ee17f2984c
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-08 21:52:49 +0530

    edit : twitter link

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -49,7 +49,7 @@ function Contact() {
                 <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
             </a>
-            <a className="socialcard">
+            <a href="https://x.com/SachulaBhavya" className="socialcard">
               <div>X /Twitter</div>
               <div>
                 <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">

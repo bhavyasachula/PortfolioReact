d09f21cf45593b63f56c7c77a63536507d76d74c
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-08 22:04:08 +0530

    edit : Added leetcode link

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -55,7 +55,7 @@ function Contact() {
                 <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
             </a>
-            <a className="socialcard">
+            <a href="https://leetcode.com/u/SabkaBaaap/" className="socialcard">
               <div>LeetCode</div>
               <div>
                 <svg width="22px" height="14px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">

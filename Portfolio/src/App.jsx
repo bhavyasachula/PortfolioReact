939192a3418bdb742808c0e074ed4636cf0e300e
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-11 22:33:35 +0530

    edit : added project titel

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -134,7 +134,10 @@ function App() {
   <span className="section-tag">SKills & Tools</span>
 </div>
   <Caraousal></Caraousal>
-    <Project></Project>
+    <div className='proheadOuter w-[100%] flex justify-center items-center'>
+      <div className='proHeading text-white w-[50%]'>Projects.</div>
+    </div>
+      <Project></Project>
   <Contact></Contact>
  </>
   )

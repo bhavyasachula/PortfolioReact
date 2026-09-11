42174238aec02ced96122aec0bfa155c7f531783
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-11 21:55:30 +0530

    edit : Adjusted border

--- Portfolio/src/components/Project.jsx
+++ Portfolio/src/components/Project.jsx
@@ -3,7 +3,7 @@ import "./Project.css"
 function Project() {
   return (
     <>
-    <div className='h-[500px] w-[100%] text-white border-1 '>
+    <div className='h-[700px] w-[100%] text-white border-1 '>
       
     </div>
     </>

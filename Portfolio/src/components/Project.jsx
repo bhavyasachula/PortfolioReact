472013a00ec649a742e681d9a1ac607f52c09aa8
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-12 22:33:28 +0530

    edit : added id here

--- Portfolio/src/components/Project.jsx
+++ Portfolio/src/components/Project.jsx
@@ -4,7 +4,7 @@ function Project() {
   return (
     <>
     <div className='projMaster flex flex-wrap h-[770px] w-[100%] mt-[15px] text-white border-1 '>
-      <div className="projSection1  h-[100%] w-[50%] p-[15px]">
+      <div className="projSection1  h-[100%] w-[50%] p-[15px]" id="projects">
 
         <div className="projTitle text-[60px]">Invoice Dispatcher Agent</div>
         <div className="projDesc text-[25px]">AgenticInvoiceDispatcher — a LangGraph single-agent system that automates invoice routing and dispatch. It uses LLM-based natural language routing (replacing brittle keyword matching) to classify incoming invoices, Tesseract OCR to extract data from them, and SMTP to send them to the right recipient/department automatically.</div>
@@ -34,13 +34,13 @@ function Project() {
           <div className="liveDemo flex w-[100%] p-[10px] h-[90px]">
             <a className="liveDemoButton" href='https://github.com/bhavyasachula/agentic-billbot-pro' target='_blank'>
               <div className="content">View Repository</div>
-              <div className="content"><svg width="35px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <div className="content"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
             </a>
             
             <a className="liveDemoButton" href='http://flowchat.streamlit.app/' target='_blank'>
               <div className="content">Live Demo</div>
-              <div className="content"><svg width="35px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <div className="content"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
  <path d="M6 18L18 6M18 6H10M18 6V14" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
             </a>
 

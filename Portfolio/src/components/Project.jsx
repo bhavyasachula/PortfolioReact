8bdec19f1b30d12a2ba3f5af413817aa3a3513ff
bhavya sachula <bhavyasachula07@gmail.com>
2026-09-18 21:27:46 +0530

    edit : got back the original code

--- Portfolio/src/components/Project.jsx
+++ Portfolio/src/components/Project.jsx
@@ -4,31 +4,51 @@ function Project({ data }) {
   const { title, desc, stats, techstack, repoLink, demoLink, steps } = data
 
   return (
+    
     <div className='projMaster flex flex-col lg:flex-row flex-wrap justify-evenly min-h-[600px] lg:h-[800px] w-full mt-[15px] text-white border-1' id="projects">
+
       <div className="projSection1 w-full lg:w-[50%] p-[15px]">
+
         <div className="projTitle text-[28px] sm:text-[36px] lg:text-[50px]">{title}</div>
+
         <div className="projDesc text-[16px] sm:text-[20px] lg:text-[25px]">{desc}</div>
 
         <div className='projStats flex flex-wrap justify-around lg:justify-around border-1 p-[10px] m-[10px] gap-[10px]'>
+
           {stats.map((s) => (
+
             <div className="statscard" key={s.label}>
+
               <div className="statsCtitle">{s.label}</div>
+
               <div className="statsvalue">{s.value}</div>
+
               <div className="statsCtitle">{s.sub}</div>
+
             </div>
+
           ))}
+
         </div>
 
         <div className='projTechstack flex-wrap'>
+
           {techstack.map((t) => (
+
             <div className="projTechstacklang" key={t}>--{t}</div>
+
           ))}
+
         </div>
 
         <div className="liveDemo flex flex-col sm:flex-row w-full p-[10px] gap-[10px] h-auto sm:h-[90px]">
+
           <a className="liveDemoButton" href={repoLink} target='_blank' rel="noreferrer">
+
             <div className="content">Repository</div>
+
             <div className="content"><ArrowIcon /></div>
+
           </a>
           <a className="liveDemoButton" href={demoLink} target='_blank' rel="noreferrer">
             <div className="content">Live Demo</div>
